d73d48b1f0c65e02c33c6f0178465d9122e4ca47
Alex Taghavi <spamdevil@protonmail.com>
2020-11-14 00:09:48 +0100

    finish project

---
+++ main.js
@@ -0,0 +1,40 @@
+//Movement Animation to happen
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+//Items
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const circle = document.querySelector('.circle');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
+//Moving Animation Event
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerWidth / 2 - e.pageY) / 25;
+    card.style.transform = `rotateX(${xAxis}deg) rotateY(${yAxis}deg)`;
+})
+//Animate In
+container.addEventListener('mouseenter', (e) => {
+    card.style.transition = `none`;
+    //Popout
+    title.style.transform = `translateZ(150px)`;
+    sneaker.style.transform = `translateZ(200px) rotateZ(-45deg)`;
+    purchase.style.transform = `translateZ(75px)`;
+    description.style.transform = `translateZ(100px)`;
+    sizes.style.transform = `translateZ(125px)`;
+    circle.style.transform = `scale(0.9)`;
+})
+
+//Animate Out
+container.addEventListener('mouseleave', (e) => {
+    card.style.transition = `all 0.5s ease`;
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+    title.style.transform = `translateZ(0px)`;
+    sneaker.style.transform = `translateZ(0px) rotateZ(0deg)`;
+    purchase.style.transform = `translateZ(0px)`;
+    description.style.transform = `translateZ(0px)`;
+    sizes.style.transform = `translateZ(0px)`;
+    circle.style.transform = `scale(1)`;
+})
